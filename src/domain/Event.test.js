dcff24185c83dce1e12724e9a88d7dd8dd4ccd52
morawskiOZ <piotrmorawski90@gmail.com>
2020-07-19 19:01:56 +0200

    test: domain Event class

---
+++ src/domain/Event.test.js
@@ -0,0 +1,48 @@
+const Event = require('../domain/Event')
+
+describe('domain Event class', () => {
+  test('should construct Event with the data passed', () => {
+    const data = {
+      email: 'test@wp.pl',
+      firstName: 'Test',
+      lastName: 'Testawski',
+      date: '1987-09-28',
+    }
+    const userEvent = new Event(data)
+    expect(userEvent.data).toStrictEqual(data)
+  })
+  test('fresh event should not have any errors', () => {
+    const data = {
+      email: 'test@wp.pl',
+      firstName: 'Test',
+      lastName: 'Testawski',
+      date: '1987-09-28',
+    }
+    const userEvent = new Event(data)
+    expect(userEvent.getErrors()).toStrictEqual([])
+  })
+  test('event with correct data after validation should not have any errors', () => {
+    const data = {
+      email: 'test@wp.pl',
+      firstName: 'Test',
+      lastName: 'Testawski',
+      date: '1987-09-28',
+    }
+    const userEvent = new Event(data)
+    userEvent.validate()
+    expect(userEvent.getErrors()).toStrictEqual([])
+  })
+  test('event with incorrect data after validation should have errors', () => {
+    const data = {
+      email: 'testwp.pl',
+      lastName: 'Testawski',
+      date: '1987ddd-09-28',
+    }
+    const userEvent = new Event(data)
+    userEvent.validate()
+    expect(userEvent.getErrors()).toStrictEqual([
+      { firstName: 'Field firstName is required' },
+      { email: 'Wrong email format' },
+    ])
+  })
+})

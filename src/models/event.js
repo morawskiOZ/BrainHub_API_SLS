2f96988855b8fd4af4e317746c7e7a212995fb14
morawskiOZ <piotrmorawski90@gmail.com>
2020-07-21 14:52:39 +0200

    refactor: reamove unneeded comments in jest config

--- src/models/event.js
+++ src/models/event.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+
 const EventSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   date: { type: Date, required: true },
 })
+
 module.exports = mongoose.model('Event', EventSchema)
